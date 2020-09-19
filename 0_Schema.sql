79479c1191a78f0ebc496fcb1abc33021cfec7ee
Hiro Suzuki <hiro.suzuki.oct10@gmail.com>
2020-09-19 21:53:57 +0900

    npopularityのSTORE化

--- 0_Schema.sql
+++ 0_Schema.sql
@@ -39,10 +39,10 @@ CREATE TABLE isuumo.chair
 
 use isuumo;
 
-ALTER TABLE estate ADD npopularity INTEGER AS (-popularity);
+ALTER TABLE estate ADD npopularity INTEGER AS (-popularity) STORED NOT NULL;
 CREATE INDEX estate_npopularity_id ON estate (npopularity, id);
 
-ALTER TABLE chair ADD npopularity INTEGER AS (-popularity);
+ALTER TABLE chair ADD npopularity INTEGER AS (-popularity) STORED NOT NULL;
 CREATE INDEX chair_npopularity_id ON chair (npopularity, id);
 
 CREATE INDEX chair_price_id ON chair (price, id);
@@ -54,3 +54,10 @@ ALTER TABLE estate ADD pt POINT AS (POINT(latitude, longitude)) STORED NOT NULL;
 CREATE SPATIAL INDEX estate_pt ON estate (pt);
 CREATE INDEX estate_latitude ON estate (latitude);
 CREATE INDEX estate_longitude ON estate (longitude);
+
+ALTER TABLE estate ADD rent_x INTEGER AS (CASE WHEN rent < 50000 THEN 0 WHEN rent < 100000 THEN 1 WHEN rent < 150000 THEN 2 ELSE 3 END) STORED NOT NULL;
+ALTER TABLE estate ADD door_width_x INTEGER AS (CASE WHEN door_width < 80 THEN 0 WHEN door_width < 110 THEN 1 WHEN door_width < 150 THEN 2 ELSE 3 END) STORED NOT NULL;
+ALTER TABLE estate ADD door_height_x INTEGER AS (CASE WHEN door_height < 80 THEN 0 WHEN door_height < 110 THEN 1 WHEN door_height < 150 THEN 2 ELSE 3 END) STORED NOT NULL;
+CREATE INDEX estate_rent_x ON estate (rent_x);
+CREATE INDEX estate_door_width_x ON estate (door_width_x);
+CREATE INDEX estate_door_height_x ON estate (door_height_x);
